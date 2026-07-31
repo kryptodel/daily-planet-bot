0b4780353132c53459a99858c7fd1be9071cda21
kryptodel <danilo43278oskdj@gmail.com>
2026-07-31 10:30:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ client.once('ready', async () => {
   try {
     console.log('Registering slash commands...');
     await rest.put(
-      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      Routes.applicationCommands(process.env.CLIENT_ID)
       { body: commands }
     );
     console.log('Slash commands registered successfully!');
